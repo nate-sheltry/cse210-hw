a1bcc718f6e40e062cddee8f8147ab8ed36175d7
Nathaniel Dunham <dun19007@byui.edu>
2023-04-25 21:07:18 -0700

    Completed the last 3 prep assignments.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        int magicNumber;
+        int answer;
+        string loop;
+        do{
+            int guesses = 0;
+            magicNumber = new Random().Next(1, 100);
+            do{
+                Console.Write("What is your guess?");
+                answer = int.Parse(Console.ReadLine());
+                guesses += 1;
+                if(answer < magicNumber){Console.WriteLine("Higher");}
+                else if(answer > magicNumber){Console.WriteLine("Lower");}
+            } while (answer != magicNumber);
+            Console.WriteLine($"You made: {guesses} guesses.");
+
+            Console.Write("Would you like to play again?(y/n) ");
+            loop = Console.ReadLine();
+        } while (loop == "y");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -2,8 +2,40 @@ using System;
 
 class Program
 {
+    static List<int> populateList()
+    {
+        List<int> list = new List<int>();
+        int input = 0;
+        while(true) {
+            try {
+            Console.Write("Enter number:");
+            input = int.Parse(Console.ReadLine());
+            if(input == 0){break;}
+            list.Add(input);
+            }
+            catch (System.Exception){
+                Console.WriteLine("Error: Please enter a integer.");
+            }
+        };
+        return list;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        int highNumber = 0;
+        int sum = 0;
+        int lowestPosNumber = 999999999;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        List<int> list = populateList();
+        foreach (int value in list){
+            sum += value;
+            if(value > highNumber){ highNumber = value;}
+            if(value < lowestPosNumber && value > 0){lowestPosNumber = value;}
+        }
+        float average = (float)sum/list.Count;
+        Console.WriteLine($"\nThe sum is: {sum}\nThe average is: {(float)sum/list.Count}\nThe largest number is: {highNumber}\n"+
+                          $"The smallest positive number is: {lowestPosNumber}\nThe sorted list is:");
+        list.Sort();
+        foreach (int value in list){Console.WriteLine(value);}
+        
     }
 }
