c36605686b7f69ab15aa751cfc5ba42aaa3818c1
Nathaniel Dunham <dun19007@byui.edu>
2023-05-16 17:11:18 -0700

    finished the fraction program.

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,19 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction test1 = new Fraction();
+        Fraction test2 = new Fraction(5);
+        Fraction test3 = new Fraction(3, 4);
+        Fraction test4 = new Fraction();
+        test4.SetTop(1); test4.SetBottom(3);
+
+        Console.WriteLine($"{test1.GetFractionString()}"
+                        + $"\n{test1.GetDecimalValue()}");
+        Console.WriteLine($"{test2.GetFractionString()}"
+                        + $"\n{test2.GetDecimalValue()}");
+        Console.WriteLine($"{test3.GetFractionString()}"
+                        + $"\n{test3.GetDecimalValue()}");
+        Console.WriteLine($"{test4.GetFractionString()}"
+                        + $"\n{test4.GetDecimalValue()}");
     }
 }
