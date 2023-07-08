9ca5dcd79f42ea5d300a66c4e11d13243d12b8cd
Nathaniel Dunham <dun19007@byui.edu>
2023-07-07 17:51:27 -0700

    finished Foundation project 1&2

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -1,9 +1,45 @@
 using System;
 
-class Program
-{
+class Program{
+    static void InitializeProgram(List<Video> videoList)
+    {
+        Comment comment1;Comment comment2;Comment comment3;
+        Video video;List<Comment> commentList;
+
+        comment1 = new Comment("JohnBaringer", "I really hate this video, it just rubbed me the wrong way.");
+        comment2 = new Comment("Tims Hardware", "If you found this video helpful check out website for mechanic tutorials!");
+        comment3 = new Comment("Alicia Downing", "Thank you so much!");
+        commentList = new List<Comment>{comment1,comment2,comment3};
+        video = new Video("John's Tutorials",
+        "How to change your brakes.", 600, commentList);
+        videoList.Add(video);
+
+        comment1 = new Comment("AlecHarding", "Beautiful cover, thank you so much!");
+        comment2 = new Comment("Garet'im", "Not the worst things I've heard...");
+        comment3 = new Comment("JohnBaringer", "At least try bro...");
+        commentList = new List<Comment>{comment1,comment2,comment3};
+        video = new Video("Bobby Drake Sings",
+        "Silent Night - Cover.", 240, commentList);
+        videoList.Add(video);
+
+        comment1 = new Comment("GoogleSpecs", "Thanks man, I was really struggling with getting my page to load fast!");
+        comment2 = new Comment("Not An Apple Buyer", "Could you do something similar but with Python?");
+        comment3 = new Comment("JSisNotGreat", "I appreciate the video, but I think the program idea presented would be better if it was done in C#.");
+        commentList = new List<Comment>{comment1,comment2,comment3};
+        video = new Video("Emanuel Codes",
+        "JavaScript Tutorials - Using IntersectionObserver to minimize CSS Load Times", 480, commentList);
+        videoList.Add(video);
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        List<Video> videoList = new List<Video>{};
+        InitializeProgram(videoList);
+        foreach(Video video in videoList)
+        {
+            video.Display();
+            Console.WriteLine();
+            video.DisplayComments();
+            Console.WriteLine();
+        }
     }
 }
--- final/Foundation2/Program.cs
+++ final/Foundation2/Program.cs
@@ -2,8 +2,45 @@ using System;
 
 class Program
 {
+    static void Initialize(List<Order> orders){
+        List<Product> products = new List<Product>(); Customer customer;
+        Address address; Product product;Order order;
+        address = new Address("123 Kings Hill Road", "Bakersfield", "VT", "USA");
+        customer = new Customer("Jack Cobb", address);
+        product = new Product("Lemon Drops", "LD1009842", 6.54, 3);
+        products.Add(product);
+        product = new Product("Starbursts", "S0097564", 3.48, 5);
+        products.Add(product);
+        product = new Product("Bagelbites", "BB0040056", 6.27, 6);
+        products.Add(product);
+        order = new Order(products, customer);
+
+        orders.Add(order);
+        products = new List<Product>();
+        address = new Address("3 Chome Kanda Kajicho", "Chiyoda City", "Tokyo", "Japan");
+        customer = new Customer("Kazumi Reitaro", address);
+        product = new Product("Matcha Kit-Kats", "MK0012093", 3.79, 5);
+        products.Add(product);
+        product = new Product("Hershey Kisses", "HK0041902", 4.23, 2);
+        products.Add(product);
+        product = new Product("Plasma", "P1060239", 3.16, 5);
+        products.Add(product);
+        order = new Order(products, customer);
+
+        orders.Add(order);
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation2 World!");
+        List<Order> orders = new List<Order>();
+        Initialize(orders);
+        foreach(Order order in orders){
+            Console.WriteLine("Packing Label");
+            Console.WriteLine(order.PackingLabel());
+            Console.WriteLine("Shipping Label");
+            Console.WriteLine(order.ShippingLabel());
+            Console.WriteLine();
+            Console.WriteLine($"Shipping Price: ${order.GetShippingPrice()}.00");
+            Console.WriteLine($"Total Price: ${Math.Round(order.CalcTotalPrice(), 2)}\n");
+        }
     }
 }
